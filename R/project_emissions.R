6b02ec510c7fd9ff5ba265b5f8e3a6dcacdfd9bc
Schuch666 <underschuch@gmail.com>
2021-12-13 16:02:08 -0500

    update for 1 region

--- R/project_emissions.R
+++ R/project_emissions.R
@@ -22,20 +22,33 @@ project_emission <- function(r, shape, factor = rep(1,nrow(shape)), background =
   region <- sf::st_transform(shape,crs = raster::projection(r))
   X      <- r
 
-  RE1    <- raster::mask(X,region[1,])
-  RE2    <- raster::mask(X,region[2,])
-  RE3    <- raster::mask(X,region[3,])
-  RE4    <- raster::mask(X,region[4,])
-  RE5    <- raster::mask(X,region[5,])
-  BACK   <- raster::mask(X,sf::st_sf(sf::st_union(region)),inverse=T)
+  if(nrow(region) == 5){
+    RE1    <- raster::mask(X,region[1,])
+    RE2    <- raster::mask(X,region[2,])
+    RE3    <- raster::mask(X,region[3,])
+    RE4    <- raster::mask(X,region[4,])
+    RE5    <- raster::mask(X,region[5,])
+    BACK   <- raster::mask(X,sf::st_sf(sf::st_union(region)),inverse=T)
 
-  FINAL <- raster::merge(factor[1] * RE1,
-                         factor[2] * RE2,
-                         factor[3] * RE3,
-                         factor[4] * RE4,
-                         factor[5] * RE5,
-                         background * BACK)
-  names(FINAL) <- names(X)
+    FINAL <- raster::merge(factor[1] * RE1,
+                           factor[2] * RE2,
+                           factor[3] * RE3,
+                           factor[4] * RE4,
+                           factor[5] * RE5,
+                           background * BACK)
+    names(FINAL) <- names(X)
+  }
+
+  if(nrow(region) == 1){
+    RE1    <- raster::mask(X,region[1,])
+    BACK   <- raster::mask(X,sf::st_sf(sf::st_union(region)),inverse=T)
+
+    FINAL <- raster::merge(factor[1] * RE1,
+                           background * BACK)
+    names(FINAL) <- names(X)
+  }
+
+  if(!nrow(region) %in% c(1,5)) stop('operation not suported')
 
   return(FINAL)
 }
