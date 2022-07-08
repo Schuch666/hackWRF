cd968480bbf2f2965e592e73c28dc9e551ea4695
schuch666 <underschuch@example.com>
2022-07-08 15:10:28 -0400

    updates in format stat

--- R/add_fit.R
+++ R/add_fit.R
@@ -7,11 +7,10 @@
 #'
 #' @export
 #'
-add_FIT <- function(file = choose.files(), verbose = F){
+add_FIT <- function(file = file.choose(), verbose = F){
   input <- read.stat(file = file)
   if('FIT (%)' %in% names(input)){
     cat(file,'already contains FIT (%)\n')
-    return()
   }else{
     fit   <- input$FA2 * 100
     write.stat(stat = cbind(input,`FIT (%)` = fit),file = file)
