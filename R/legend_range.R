3b8230ff269ae133dd7d09da5775b1cb709c4b20
schuch666 <underschuch@example.com>
2022-09-01 15:06:48 -0400

    update in legend_range including y

--- R/legend_range.R
+++ R/legend_range.R
@@ -1,6 +1,7 @@
 #' Get the distance in kilometers between two points.
 #'
 #' @param x raster or array
+#' @param y raster or array to mean (x is used only for the range in this case)
 #' @param text.width Longitude in decimals
 #' @param dig vector with number of digits for plot
 #' @param xjust passed to legend
@@ -9,6 +10,7 @@
 #' @param y.intersp passed to legend
 #' @param show.mean set TRUE to hide mean value
 #' @param unit a string for units
+#' @param label_mean label in case y is provided
 #'
 #' @note for use with raster use before any change of projection
 #' @note text.width can vary depending on map dimensions
@@ -26,13 +28,15 @@
 #'
 #' @export
 #'
-legend_range <- function(x, text.width=6, dig = c(2,2,2),
+legend_range <- function(x, y,
+                         text.width=6, dig = c(2,2,2),
                          xjust = 0.5,
                          horiz = TRUE,
                          y.intersp =0.1,
                          x.intersp =0.1,
                          show.mean = TRUE,
-                         unit = ""){
+                         unit = "",
+                         label_mean = 'ALL:'){
 
   if(class(x)[1] == 'Raster' ||
      class(x)[1] == 'RasterLayer' ||
@@ -40,9 +44,22 @@ legend_range <- function(x, text.width=6, dig = c(2,2,2),
     x <- raster_to_ncdf(x)
   }
 
+  if(!missingArg(y)){
+    if(class(y)[1] == 'Raster' ||
+       class(y)[1] == 'RasterLayer' ||
+       class(y)[1] == 'RasterBrick'){
+      y <- raster_to_ncdf(y)
+    }
+  }
+
   mi <- paste('Min:', formatC(min(x, na.rm = TRUE), digits = dig[1], format = "f"),unit)
-  me <- paste('Mean:',formatC(mean(x,na.rm = TRUE), digits = dig[2], format = "f"),unit)
+  if(missingArg(y)){
+    me <- paste('Mean:',formatC(mean(x,na.rm = TRUE), digits = dig[2], format = "f"),unit)
+  }else{
+    me <- paste(label_mean,formatC(mean(y,na.rm = TRUE), digits = dig[2], format = "f"),unit)
+  }
   ma <- paste('Max:', formatC(max(x, na.rm = TRUE), digits = dig[3], format = "f"),unit)
+
 
   if(show.mean){
     le <- c(mi,me,ma)
