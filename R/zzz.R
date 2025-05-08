39b4967a1a19d5732a8ac68f3d999b4d6c03ca9d
Schuch666 <underschuch@gmail.com>
2025-05-08 14:23:28 -0400

    added message on startup

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("This package is not under development, recomended to change to eva3dm package https://schuch666.github.io/eva3dm\nInstallation from CRAN:\n install.packages('eva3dm')\nInstallation from GitHub:\n remotes::install_github('Schuch666/eva3dm')")
+}
